1d2937e2f0a9ae67abf5ea936e5d1d3f1ab894ac
ivantabera <ivantabera19@gmail.com>
2021-08-04 13:02:39 -0500

    use State en un contador, y desestructuracion para poder modificar una propiedad del objeto, uso del operador de arrastre para hacer una copia del estado anterior al nuevo estado

---
+++ src/components/01-useState/CounterApp.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import './counter.css';
+
+export const CounterApp = () => {
+
+    const [ state, setState] = useState({
+        counter1:10,
+        counter2:20,
+        counter3:30,
+        counter4:40,
+        counter5:50
+    });
+    
+    /* Desestructuramos para poder ocupar la propiedad del objeto state que se quiera modificar, 
+    sin tener que modificar todas las propiedades */
+    const { counter1, counter2 } = state;
+
+    return (
+        <>
+            <h1>Counter1 { counter1 }</h1>
+            <h1>Counter2 { counter2 }</h1>
+            <hr />
+            <button 
+                className="btn btn-primary"
+                onClick= { () => {
+                    setState({
+                        /* Se utiliza el operador de arrastre (...) del estado anterior para 
+                        crear una copia de todos los valores anteriores y sean pasados al nuevo state */
+                        ...state,
+                        counter1 : counter1 + 1
+                    });
+                }}
+            >
+                +1
+            </button>
+        </>
+    )
+}
