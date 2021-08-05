3983bbfe2bbd536a1889377312a8aa7481b150d1
ivantabera <ivantabera19@gmail.com>
2021-08-04 21:19:21 -0500

    se realizo el formulario con un custom hook

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import ReactDOM from 'react-dom';
 // import { HookApp } from './HookApp';
 // import { CounterApp } from './components/01-useState/CounterApp.js';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
-import { SimpleForm } from './components/02-useEfect/SimpleForm';
+// import { SimpleForm } from './components/02-useEfect/SimpleForm';
+import { FormWithCustomHook } from './components/02-useEfect/FormWithCustomHook';
 
 
 
 ReactDOM.render(
-  <SimpleForm />,
+  <FormWithCustomHook />,
   document.getElementById('root')
 );
